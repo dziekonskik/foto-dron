0e4de20fbae466e226d7adff2143508b8fe995b8
Seven <jaseveen@gmail.com>
2020-03-27 01:40:08 +0100

    reworked slider(screen) add gsap, add works section basic animations and functionalities

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,32 @@
-// Welcome to my slider!!
+// -----=======WELCOME TO MY SLIDER=======-----
 
 const container = document.querySelector('.slider__container');
+const screen = container.querySelector('.slider__screen')
 const prevButton = container.querySelector('.slider__prevButton');
 const nextButton = container.querySelector('.slider__nextButton');
 let counter = 0;
 
 function handlePrevButton() {
     counter--;
-    container.style.backgroundImage = `url(../slider/DJI_${counter}.jpg)`;
+    gsap.fromTo(".slider__screen", { opacity: 0}, { opacity: 1, duration: .2, ease: Elastic})
+    screen.style.backgroundImage = `url(../slider/DJI_${counter}.jpg)`;
     if (counter < 1 ) {
-        container.style.backgroundImage = 'url(../slider/DJI_7.jpg)';
+        screen.style.backgroundImage = 'url(../slider/DJI_7.jpg)';
         counter = 7;
     }
 }
 
 function handleNextButton() {
     counter++;
-    container.style.backgroundImage = `url(../slider/DJI_${counter}.jpg)`;
+    gsap.fromTo(".slider__screen", { opacity: 0 }, { opacity: 1, duration: .2})
+    screen.style.backgroundImage = `url(../slider/DJI_${counter}.jpg)`;
     if (counter > 7 ) {
-        container.style.backgroundImage = 'url(../slider/DJI_1.jpg)';
+        screen.style.backgroundImage = 'url(../slider/DJI_1.jpg)';
         counter = 1;
     }
 }
 
 prevButton.addEventListener('click', handlePrevButton);
 nextButton.addEventListener('click', handleNextButton);
+
 
