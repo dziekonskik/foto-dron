909b7b8475a926afe5b9f5df025f76aefe65148c
Seven <jaseveen@gmail.com>
2020-07-11 22:32:01 +0200

    add finished styles ad temporary js for open/closed hamburger meu for mobile

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,1 +1,7 @@
-console.log('hamburger');
+const hamburger = document.querySelector('.header__hamburger');
+
+function handleHamburger(e) {
+  e.target === hamburger ? hamburger.classList.toggle('open') : null;
+}
+
+hamburger.addEventListener('click', handleHamburger);
