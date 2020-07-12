c6eeb9d39fdcf75a12d0b4baf59e2454679d4593
Seven <jaseveen@gmail.com>
2020-07-12 23:21:12 +0200

    add finished hamburger styles, new call2a btns, button animation for open menu

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector('.header__hamburger');
+const leftButton = document.querySelector('.call2a-1');
+const rightButton = document.querySelector('.call2a-2');
 
 function handleHamburger(e) {
-  e.target === hamburger ? hamburger.classList.toggle('open') : null;
+  if (e.target === hamburger) {
+    hamburger.classList.toggle('active');
+    leftButton.classList.toggle('animate-left');
+    rightButton.classList.toggle('animate-right');
+  }
 }
 
 hamburger.addEventListener('click', handleHamburger);
