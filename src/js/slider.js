bdd5af6c36da17c6b140877735951929be7526a2
Seven <jaseveen@gmail.com>
2020-07-13 14:59:08 +0200

    add slider touch test

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,34 +1,47 @@
 // -----=======WELCOME TO MY SLIDER=======-----
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('.slider__container');
+  const screen = container.querySelector('.slider__screen');
+  const prevButton = container.querySelector('.slider__prevButton');
+  const nextButton = container.querySelector('.slider__nextButton');
+  let counter = 0;
 
-const container = document.querySelector('.slider__container');
-const screen = container.querySelector('.slider__screen');
-const prevButton = container.querySelector('.slider__prevButton');
-const nextButton = container.querySelector('.slider__nextButton');
-let counter = 0;
-
-function handlePrevButton() {
-  counter--;
-  gsap.fromTo(
-    '.slider__screen',
-    { opacity: 0 },
-    { opacity: 1, duration: 0.2, ease: Elastic }
-  );
-  screen.style.backgroundImage = `url(img/slider/DJI_${counter}.jpg)`;
-  if (counter < 1) {
-    screen.style.backgroundImage = 'url(img/slider/DJI_7.jpg)';
-    counter = 7;
+  function handlePrevButton() {
+    counter--;
+    gsap.fromTo(
+      '.slider__screen',
+      { opacity: 0 },
+      { opacity: 1, duration: 0.2, ease: Elastic }
+    );
+    screen.style.backgroundImage = `url(img/slider/DJI_${counter}.jpg)`;
+    if (counter < 1) {
+      screen.style.backgroundImage = 'url(img/slider/DJI_7.jpg)';
+      counter = 7;
+    }
   }
-}
 
-function handleNextButton() {
-  counter++;
-  gsap.fromTo('.slider__screen', { opacity: 0 }, { opacity: 1, duration: 0.2 });
-  screen.style.backgroundImage = `url(img/slider/DJI_${counter}.jpg)`;
-  if (counter > 7) {
-    screen.style.backgroundImage = 'url(img/slider/DJI_1.jpg)';
-    counter = 1;
+  function handleNextButton() {
+    counter++;
+    gsap.fromTo(
+      '.slider__screen',
+      { opacity: 0 },
+      { opacity: 1, duration: 0.2 }
+    );
+    screen.style.backgroundImage = `url(img/slider/DJI_${counter}.jpg)`;
+    if (counter > 7) {
+      screen.style.backgroundImage = 'url(img/slider/DJI_1.jpg)';
+      counter = 1;
+    }
   }
-}
 
-prevButton.addEventListener('click', handlePrevButton);
-nextButton.addEventListener('click', handleNextButton);
+  prevButton.addEventListener('click', handlePrevButton);
+  nextButton.addEventListener('click', handleNextButton);
+
+  const form = document.forms[0];
+  console.log(form);
+  function obczaj(e) {
+    console.log(e.touches);
+  }
+
+  screen.addEventListener('touchstart', obczaj);
+});
