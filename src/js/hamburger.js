660dc158322a80fe55d8c54d03a58a6c24b5f557
Seven <jaseveen@gmail.com>
2020-07-13 10:21:37 +0200

    add styles to nav, menu and call2a btns

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,12 +1,14 @@
 const hamburger = document.querySelector('.header__hamburger');
 const leftButton = document.querySelector('.call2a-1');
 const rightButton = document.querySelector('.call2a-2');
+const ul = document.querySelector('.header__nav');
 
 function handleHamburger(e) {
   if (e.target === hamburger) {
     hamburger.classList.toggle('active');
     leftButton.classList.toggle('animate-left');
     rightButton.classList.toggle('animate-right');
+    ul.classList.toggle('show-menu');
   }
 }
 
